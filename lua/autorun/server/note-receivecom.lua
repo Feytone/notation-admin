ee7336c57ed3a6139c0e90a3c6d77468175d1906
Feytone <stancalmettes@hotmail.fr>
2017-11-06 18:08:18 +0100

    Added all queries (sql) serverside.

--- lua/autorun/server/note-receivecom.lua
+++ lua/autorun/server/note-receivecom.lua
@@ -1,5 +1,12 @@
 util.AddNetworkString("notecomp")
 util.AddNetworkString("notecomp-admin")
+util.AddNetworkString("validnoteadd_notation")
+util.AddNetworkString("sendtoreceive_notation")
+util.AddNetworkString("sendnono_notation")
+
+if !sql.TableExists("notes_admin") then
+    sql.Query("CREATE TABLE notes_admin ( SteamID TEXT, Note SMALLINT(50000) UNSIGNED, Nombre SMALLINT(50000) UNSIGNED )") print("Table créée tout en haut")
+end
 
 hook.Add("PlayerSay","CommandMtx",function(ply,text)
 
@@ -16,12 +23,38 @@ hook.Add("PlayerSay","CommandMtx",function(ply,text)
 	end
 	if ConfigCommandeAdmin == text and ply:IsValid() and ply:IsPlayer() and RestricGroupsShow[ply:GetUserGroup()] then
 		print("La commande a fonctionné")
+
 		net.Start("notecomp-admin")
+		net.WriteTable(sql.Query("SELECT SteamID FROM notes_admin"))
+		net.WriteString(sql.QueryValue("SELECT Note FROM notes_admin WHERE SteamID='"..v.."'"))
 		net.Send(ply)
 		else print("La commande n'a pas fonctionné")
 	end
 end)
 
-if !sql.TableExists("notes_admin") then
-    sql.Query("CREATE TABLE notes_admin ( SteamID TEXT, Note VARCHAR(1), Nombre VARCHAR(50000) UNSIGNED )") print("Table créée tout en haut")
-end
+
+
+net.Receive("validnoteadd_notation", function(len,ply)
+
+	local notead = tonumber(net.ReadString())
+	local adminpl = net.ReadEntity()
+
+	if !sql.TableExists("notes_admin") then
+    	sql.Query("CREATE TABLE notes_admin ( SteamID TEXT, Note SMALLINT(50000) UNSIGNED, Nombre SMALLINT(50000) UNSIGNED )") print("Table créée")
+    else print("table déjà créée")
+    end
+    sql.Query("INSERT OR IGNORE INTO notes_admin VALUES( SteamID='" .. adminpl:SteamID() .. "' )") 
+    sql.Query("REPLACE INTO notes_admin SET Note=Note+'"..notead.."', Nombre=Nombre+1 WHERE SteamID='"..adminpl:SteamID().."' ")
+
+end)
+
+net.Receive("sendtoreceive_notation", function(len,ply)
+
+	local noteee = sql.QueryValue("SELECT Note FROM notes_admin WHERE SteamID='"..v.."'")
+	local nombre = sql.QueryValue("SELECT Nombre FROM notes_admin WHERE SteamID='"..v.."'")
+	net.Start("sendnono_notation")
+	net.WriteString(noteee)
+	net.WriteString(nombre)
+	net.Send(ply)
+
+end)
